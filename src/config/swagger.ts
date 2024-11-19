7fe9e9fc8d91dfe76fd9d21dda1fa39810bfb288
Dennys Matos <dennyscaetano@yahoo.com.br>
2024-11-19 14:22:50 -0300

    first commit

---
+++ src/config/swagger.ts
@@ -0,0 +1,25 @@
+export const swaggerOptions = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'Blog API',
+      version: '0.1.0',
+      description: 'API para estudo de testes de API',
+      license: {
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'Dennys Matos',
+        url: 'https://github.com/dennyscaetano',
+        email: 'dennyscaetano@yahoo.com.br',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['**/*.yml'],
+}
